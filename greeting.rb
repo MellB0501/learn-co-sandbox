79dc7756f8f1edf464bfdd89ff9e1c1a1c3e10a0
Mell Basham <mellbasham@gmail.com>
2021-01-26 23:57:53 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,8 @@
+
+def greeting_programmer(name, language)
+  puts "Hello, #{name}. We heard your are great #{language} programmer."
+end
+
+greeting_programmer("Maria, Ruby")
+
+greeting_programmer("Steven, Elixir")
